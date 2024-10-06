d98ab78c5f5c8883775ac82129c9b82686b5983b
Vilas-cp <vilaspgowda1000@gmail.com>
2024-10-06 11:30:30 +0530

    header updated

--- client/src/app/habitable-calculator/components/Header.js
+++ client/src/app/habitable-calculator/components/Header.js
@@ -6,18 +6,26 @@ import Link from 'next/link';
 export default function Header() {
  
   return (
-    <div className='bg-black flex justify-between items-center h-20 p-4  fixed z-10 w-full px-[150px]'>
-      <div className='flex gap-20 text-gray-300'>
-      <Link href={'/'} className='font-bold '>
-        <span className='text-[#3077c6] text-[20px]'>Expo</span>Planet
-      </Link>
+    <div className="h-[15vh] bg-black p-4 z-10 w-full px-[150px] flex justify-between items-center">
+    <div className="flex gap-20 text-gray-300">
+      <a target="_blank" href={"/"} className="font-bold ">
+        <span className="text-[#3077c6] text-[20px]">Expo</span>Planet
+      </a>
       <ul className="flex gap-6 list-none font-medium">
-        <Link href={'/exo-planet-space'}>Experience the space</Link>
-        <Link href={'/habitable-calculator'}>Habitable calculator</Link>
-     
+        <a target="_blank" href={"/exo-planet-space"}>
+          Experience the space
+        </a>
+        <a target="_blank" href={"/habitable-calculator"}>
+          Habitable calculator
+        </a>
+        <a target="_blank" href={"/orbit-rotation"}>
+          {"Orbit Rotation (Test)"}
+        </a>
+        <a target="_blank" href={"/interactive"}>
+          {"Interactivity Sphere (Test)"}
+        </a>
       </ul>
-      </div>
-       
     </div>
+  </div>
   )
 };
