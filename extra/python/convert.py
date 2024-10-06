624ab01a8fde3b57d14cce4d24b6970409b1824a
ManojaD2004 <manojadkc2004@gmail.com>
2024-10-07 00:12:56 +0530

    Clean Table and Result.Json

--- extra/python/convert.py
+++ extra/python/convert.py
@@ -1,9 +1,22 @@
+import csv
+import json
+
 if __name__ == "__main__":
     data = {}
     csvFilePath = "./table.csv"
+    csvFilePath = "./table_clean"
     jsonFilePath = "./data.json"
+    with open(csvFilePath, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
+        for line in infile:
+            if not line.strip().startswith('#'):
+                outfile.write(line)
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
+        for row in csvReader:
+            print(row)
+            if row and not row[0].startswith("#"):
+                csvReader = csv.DictReader(csvf, fieldnames=row)
+                break
         for rows in csvReader:
             key = rows['rowid']
             data[key] = rows
